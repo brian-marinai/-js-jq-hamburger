82edfafed9a2afc22d8e8c9d83c8f200ec535aa8
Brian Marinai <brianmarinai.bm@gmail.com>
2020-07-02 16:39:35 +0200

    commit con esercizio in corso

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$("a").click(
+  function() {
+    $("").addClass("hamburger-menu active");
+    console.log("ho cliccato");
+  }
+);
+
+
+
+// $("i").click(
+//   function() {
+//     $("hamburger-menu").show();
+//     console.log("ho cliccato");
+//   }
+// );
+
+// $("a").click(
+//   function() {
+//     $("hamburger-menu").fadeIn(1000);
+//   }
+//   );
+
+
+console.log("inizio");
